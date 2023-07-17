e8b70ad7cf613d2683e9ecc8451627eab34fdb0a
laughing-nerd <riturajpaul99@gmail.com>
2023-07-17 15:00:39 +0530

    Handle multiple clients

--- client.py
+++ client.py
@@ -1,13 +1,19 @@
+import os
 import socket
+import os
 
-DOWNLOAD_FILE_PATH="./Downloads/test.txt"
+DOWNLOAD_FILE_PATH = os.getcwd() + "/Downloads"
 # HOST = "localhost"
 PORT = 9090
 try:
     HOST = input("Enter the HOST IP: ")
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-    client.connect((HOST, PORT)) #Connect to the server running on localhost
+    client.connect((HOST, PORT)) #Connect to the server running on local host
 
+    #Create a download directory for the client
+    if not os.path.isdir(DOWNLOAD_FILE_PATH):
+        os.mkdir(DOWNLOAD_FILE_PATH)
+    
     #Only GET instruction is supported
     while True:
         instruction = input("Enter the operation: ")
@@ -25,7 +31,7 @@ try:
             break
         data = data+received_data
 
-    with open(DOWNLOAD_FILE_PATH, "w") as file:
+    with open(DOWNLOAD_FILE_PATH+"/test.txt", "w") as file:
         file.write(data)
         print("Download successful")
     
--- server.py
+++ server.py
@@ -1,27 +1,33 @@
 import socket
+import threading 
 
 FILE_PATH = "./data/test.txt"
 HOST = ""
 PORT = 9090
+
+def handleConnections(client, addr):
+    print(f"{addr} is now connected to me!")
+    
+    instruction = client.recv(3).decode() #Receive instruction from the client. Currently only GET is supported
+       
+    if(instruction=="GET"):
+        with open(FILE_PATH, "r") as file:
+            data = file.read()
+            data = data+"<EOF>"
+        client.sendall(data.encode())
+        print(f"File sent to {addr}")
+
 try:
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT)) #Only localhost clients can connect to this server
-    server.listen(1) #Listen for incoming connections
+    server.bind((HOST, PORT))
+    server.listen(5) #Listen for incoming connections
     print(f"Server is listening on port {PORT}")
 
     while True:
-        client, addr = server.accept(); #Catch the incoming connection
-        print(f"{addr} is now connected to me!")
-
-        instruction = client.recv(3).decode() #Receive instruction from the client. Currently only GET is supported
+        client, addr = server.accept(); #Catch the incoming connections
+        thread = threading.Thread(target=handleConnections, args=(client, addr))
+        thread.start()
         
-        if(instruction=="GET"):
-            with open(FILE_PATH, "r") as file:
-                data = file.read()
-            data = data+"<EOF>"
-            client.sendall(data.encode())
-            print("Sent file to the client")
-
 except Exception as e:
     print("Connection terminated!")
     print(e)
